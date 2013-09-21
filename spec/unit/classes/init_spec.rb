eb3155bff154f62029698004cade8589fb1cd469
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2013-09-21 11:16:43 -0400

    Update to work with puppetlabs/postgresql > 2.5.0

--- spec/unit/classes/init_spec.rb
+++ spec/unit/classes/init_spec.rb
@@ -6,8 +6,9 @@ describe 'puppetdb', :type => :class do
   context 'on a supported platform' do
     let(:facts) do
       {
-        :osfamily => 'RedHat',
-        :postgres_default_version => '9.1',
+        :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :operatingsystemrelease => '6.0',
         :concat_basedir => '/var/lib/puppet/concat',
       }
     end
@@ -20,8 +21,9 @@ describe 'puppetdb', :type => :class do
   context 'with invalid arguments on a supported platform' do
     let(:facts) do
       {
-        :osfamily => 'RedHat',
-        :postgres_default_version => '9.1',
+        :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :operatingsystemrelease => '6.0',
         :concat_basedir => '/var/lib/puppet/concat',
       }
     end
